d1ceb79481add93f129709ef4844058b618276fb
Jaime Yera <jaimeyera@gmail.com>
2021-01-04 18:41:19 +0100

    Disconect from config and eureka architecture

--- src/main/java/com/microservices/micro/warehouse/warehouseservice/controller/WarehouseController.java
+++ src/main/java/com/microservices/micro/warehouse/warehouseservice/controller/WarehouseController.java
@@ -1,5 +1,6 @@
 package com.microservices.micro.warehouse.warehouseservice.controller;
 
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,6 +10,8 @@ import com.microservices.micro.warehouse.warehouseservice.service.WarehouseServi
 
 @RestController
 public class WarehouseController {
+	
+	private final RabbitTemplate rabbitTemplate = new RabbitTemplate();
 	
 	@Autowired
 	private WarehouseService service;
@@ -21,6 +24,12 @@ public class WarehouseController {
 	@GetMapping("/warehouse/{name}")
 	public String getWarehouseByName(@PathVariable("name") String name) {
 		return service.getWarehouseByName(name).toString();
+	}
+	
+	@GetMapping("/warehouse/send/{msg}")
+	public String getSendMessage(@PathVariable("msg") String msg) {
+		rabbitTemplate.convertAndSend("warehouseexchange","warehouse.stock.test",msg);
+		return "We send a message";
 	}
 
 }
